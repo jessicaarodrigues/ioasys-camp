1546450c94fd8e1c2dbb7c7e7c731318ae64d027
jessicaarodrigues <j.alexandra.r61@gmail.com>
2022-01-19 21:39:28 -0300

    Adiciona duas pastas e três novos arquivos

---
+++ Módulo 3/info.js
@@ -0,0 +1,5 @@
+import logger from './logger.js';
+
+logger.log("Log");
+logger.error("Error");
+logger.info("Info");
---
+++ Módulo 3/logger.js
@@ -0,0 +1,6 @@
+const error = (message) => console.error(message)
+const log = (message) => console.log(message)
+const info = (message) => console.info(message)
+
+
+export default { error, log, info };
