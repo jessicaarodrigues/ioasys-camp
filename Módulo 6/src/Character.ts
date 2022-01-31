ef72a966569ab8da37fd97a243ce763370ea2d95
jessicaarodrigues <j.alexandra.r61@gmail.com>
2022-01-31 16:35:24 -0300

    adiciona pasta e arquivos Módulo 6

---
+++ Módulo 6/src/Character.ts
@@ -0,0 +1,17 @@
+class Character {
+    name: string;
+    energy: number;
+    life: number;
+    attack: number;
+    defense: number;
+
+    constructor() {
+        this.name = "Jessica";
+        this.energy = 80;
+        this.life = 100;
+        this.attack = 80;
+        this.defense = 100;
+    }
+}
+
+export default Character;
---
+++ Módulo 6/src/index.ts
@@ -0,0 +1,5 @@
+import Character from "./Character";
+
+const human = new Character
+
+console.log('Human [Character]', human);
